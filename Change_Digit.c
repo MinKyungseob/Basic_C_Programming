86f38d4fc74f610e58baec12ff3c3a8d0aa9b18b
Min <44631060+MinKyungseob@users.noreply.github.com>
2023-01-12 14:31:43 +0900

    Update and rename digit.c to Change_Digit.c

--- Change_Digit.c
+++ Change_Digit.c
@@ -1,10 +1,11 @@
+//numbers under 512, change 10 digit nums into other digit way, like decimal number to binary number
 #include<stdio.h>
 
 int main() {
 	int num, times, sum = 0;
 	scanf_s("%d", &num);
 	if (num >= 512)
-		return printf("512 ÀÌ»ó ÀÔ·Â. ¹üÀ§¸¦ ¹ş¾î³µ½À´Ï´Ù.\n Range over 512 ");
+		return printf("512 ì´ìƒ ì…ë ¥. ë²”ìœ„ë¥¼ ë²—ì–´ë‚¬ìŠµë‹ˆë‹¤.\n Range over 512 "); //Korean: number is over 512, it's out of range
 	scanf_s("%d", &times);
 	int digit = 10;
 	for (int i = 0; i <= 10; i++)
